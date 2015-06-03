a873e64e47f9c7994e6a90f6df3f7e28ad2ce0ba
Colin Clark <colinbdclark@gmail.com>
2015-06-02 23:24:08 -0400

    Discards outliers from calcuated average. Runs batches in sets of 2000.

--- js/sheep.js
+++ js/sheep.js
@@ -14,61 +14,68 @@ var sheep = sheep || {};
 
 (function () {
     "use strict";
-    
+
     var p = window.performance;
     if (!p) {
         window.performance = Date;
     } else if (!p.now) {
         p.now = p.webkitNow || p.oNow || p.msNow;
     }
-    
+
     var finishTestSequence = function (timing, spec) {
-        timing.avg = timing.total / spec.numReps;
+        // Total up all the samples, disregarding the outliers.
+        for (var i = 0; i < spec.numReps; i++) {
+            timing.total += timing.samples[i];
+        }
+        timing.total -= timing.max;
+        timing.total -= timing.min;
+
+        timing.avg = timing.total / (spec.numReps - 2);
         timing.runs = spec.numReps;
         timing.name = spec.name;
         spec.onSuccess(timing);
     };
-    
+
     var runTest = function (obj, timing, testFn, onTestComplete) {
         var start = window.performance.now(),
             end,
             dur;
-        
+
         testFn(obj);
         end = window.performance.now();
         dur = end - start;
-        timing.total += dur;
+        timing.samples.push(dur);
         timing.max = dur > timing.max ? dur : timing.max;
         timing.min = dur < timing.min ? dur : timing.min;
         if (onTestComplete) {
             onTestComplete(timing);
         }
-        
+
         return timing;
     };
-    
+
     var runTestBatch = function (numReps, obj, timing, testFn, onBatchComplete) {
         for (var i = 0; i < numReps; i++) {
             runTest(obj, timing, testFn);
         }
-        
+
         if (onBatchComplete) {
             onBatchComplete(timing);
         }
-        
+
         return timing;
     };
-    
+
     var runTestSequenceSync = function (obj, timing, spec) {
         runTestBatch(spec.numReps, obj, timing, spec.test);
         finishTestSequence(timing, spec);
         return timing;
     };
-    
+
     var runTestSequenceAsync = function (obj, timing, spec) {
-        var batchSize = 100,
+        var batchSize = 2000,
             i = 0;
-        
+
         var asyncTest = function () {
             runTestBatch(batchSize, obj, timing, spec.test, function () {
                 i += batchSize;
@@ -79,10 +86,10 @@ var sheep = sheep || {};
                 }
             });
         };
-        
+
         asyncTest();
     };
-    
+
     var renderTestRow = function (table, timing) {
         var row = document.createElement("tr");
         table.appendChild(row);
@@ -93,13 +100,13 @@ var sheep = sheep || {};
             timing.avg.toFixed(5) + "</td><td>" +
             timing.min.toFixed(5) + "</td><td>" +
             timing.max.toFixed(5) + "</td>";
-        
+
         return row;
     };
-    
+
     var renderTable = function (container) {
         var table = document.createElement("table");
-        
+
         table.innerHTML = "<tr>" +
             "<th>Name</th>" +
             "<th>Runs</th>" +
@@ -109,10 +116,10 @@ var sheep = sheep || {};
             "<th>Max</th>" +
             "</tr>";
         container.appendChild(table);
-        
+
         return table.querySelector("tbody");
     };
-    
+
     var renderStatusArea = function (table, numTests) {
         var statusArea = document.createElement("tr");
         statusArea.className = "sheep-statusArea";
@@ -120,7 +127,7 @@ var sheep = sheep || {};
 	        " <span class='sheep-currentTest'>0</span> of" +
 	        " <span class='sheep-numTests'>" + numTests + "</span></p></td>";
 	    table.appendChild(statusArea);
-	    
+
         var spinner = new Spinner({
             color: "#000",
             hwaccel: true,
@@ -130,42 +137,43 @@ var sheep = sheep || {};
         spinner.spin(statusArea.querySelector("td"));
         return statusArea;
     };
-    
+
     var updateStatusArea = function (currentTestNum) {
         document.querySelector(".sheep-currentTest").innerHTML = currentTestNum;
     };
-    
+
     var renderResultsView = function (numTests) {
         var container = document.createElement("div");
         var table = renderTable(container);
         var statusArea = renderStatusArea(table, numTests);
         document.querySelector("body").appendChild(container);
-	    
+
         return {
             container: container,
             table: table,
             statusArea: statusArea
         };
     };
-    
+
     sheep.runTestSpec = function (spec, async) {
         spec.numReps = spec.numReps || 10000;
         spec.async = async;
-        
+
         if (typeof (spec) === "function") {
             spec = {
                 test: spec
             };
         }
-        
+
         var obj = spec.setup ? spec.setup() : undefined,
             timing = {
                 total: 0,
+                samples: [],
                 max: 0,
                 min: Infinity,
                 async: async
             };
-        
+
         var testSequenceFn = async ? runTestSequenceAsync : runTestSequenceSync;
         return testSequenceFn(obj, timing, spec);
     };
@@ -173,22 +181,19 @@ var sheep = sheep || {};
     sheep.test = function (testSpecs, async, onAllTestsComplete) {
         testSpecs = typeof (testSpecs.length) === "number" ? testSpecs : [testSpecs];
         async = async === undefined || async === null || async  === true ? true : false;
-	    
-        // And asynchronously run all test sets.
-	    var markup = renderResultsView(testSpecs.length),
-	        allTimes = [],
-	        nextTest = 0,
-	        runNextTest,
-	        testSuccess;
-	    
-        runNextTest = function () {
+
+        var markup = renderResultsView(testSpecs.length),
+            allTimes = [],
+            nextTest = 0;
+
+        function runNextTest() {
             var spec = testSpecs[nextTest];
             updateStatusArea(nextTest + 1);
 	        spec.onSuccess = testSuccess;
 	        sheep.runTestSpec(spec, async);
-        };
-        
-		testSuccess = function (timing) {
+        }
+
+        function testSuccess (timing) {
             nextTest++;
             allTimes.push(timing);
             markup.table.appendChild(renderTestRow(markup.table, timing));
@@ -200,8 +205,8 @@ var sheep = sheep || {};
                     onAllTestsComplete();
                 }
             }
-        };
-        
+        }
+
         // Start the test sequence.
         if (testSpecs.length > 0) {
             runNextTest();
